62255aa3deade83f764c67031d8dc65a4a22889c
Will Ho <will.ho@grabtaxi.com>
2022-02-11 16:07:26 +0800

    Add application change mapping logic

--- cmd/argonaut/main.go
+++ cmd/argonaut/main.go
@@ -3,12 +3,18 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
+	"path"
+	"path/filepath"
+	"strings"
 
 	"github.com/go-git/go-git/v5"
 	gitconf "github.com/go-git/go-git/v5/config"
 	"github.com/go-git/go-git/v5/plumbing"
 	"github.com/go-git/go-git/v5/plumbing/transport/http"
+	"github.com/ikaruswill/argonaut/internal/argocd"
 	"github.com/ikaruswill/argonaut/internal/config"
+	"gopkg.in/yaml.v2"
 )
 
 func main() {
@@ -27,7 +33,7 @@ func main() {
 	// Logic flow
 	// Load Config
 	// Get git changed files
-	// Sort change list by directory tree
+	// Get unique directories
 	// For each changed directory
 	// - Check if directory contains Application definitions
 	// - Determine if Application is helm, kustomize or plain yaml
@@ -121,31 +127,91 @@ func main() {
 	}
 
 	// Iterate through patches
-	newFiles := []string{}
-	modifiedFiles := []string{}
+	changedFiles := []string{}
 	deletedFiles := []string{}
-	renamedFromFiles := []string{}
-	renamedToFiles := []string{}
 	filePatches := patch.FilePatches()
 	fmt.Printf("Length of patches: %d\n", len(filePatches))
 	for _, filePatch := range filePatches {
 		fromFile, toFile := filePatch.Files()
 		if fromFile == nil {
 			fmt.Printf("[+] %s\n", toFile.Path())
-			newFiles = append(newFiles, toFile.Path())
+			changedFiles = append(changedFiles, toFile.Path())
 		} else if toFile == nil {
 			fmt.Printf("[-] %s\n", fromFile.Path())
 			deletedFiles = append(deletedFiles, fromFile.Path())
 		} else {
 			if fromFile.Path() == toFile.Path() {
 				fmt.Printf("[~] %s\n", fromFile.Path())
-				modifiedFiles = append(modifiedFiles, fromFile.Path())
+				changedFiles = append(changedFiles, fromFile.Path())
 			} else {
 				fmt.Printf("[~] %s to %s", fromFile.Path(), toFile.Path())
-				renamedFromFiles = append(renamedFromFiles, fromFile.Path())
-				renamedToFiles = append(renamedToFiles, toFile.Path())
+				// Only use toFile since fromFile no longer exists due to rename
+				changedFiles = append(changedFiles, toFile.Path())
 			}
 		}
 	}
 
+	// Get unique directories
+	dirMap := map[string][]string{}
+	for _, p := range changedFiles {
+		dir, file := path.Split(p)
+		dirMap[dir] = append(dirMap[dir], file)
+	}
+
+	// Discover all files
+	appCache := map[string]argocd.Application{}
+	resourceAppPathMap := map[string]string{}
+	filepath.WalkDir(".", func(path string, d os.DirEntry, err error) error {
+		ext := filepath.Ext(path)
+		if ext == ".yaml" || ext == ".yml" {
+			app := argocd.Application{}
+			f, err := os.ReadFile(path)
+			if err != nil {
+				log.Printf("Fail os.ReadFile for %s: %s", path, err.Error())
+				return err
+			}
+			yaml.Unmarshal(f, &app)
+			if strings.HasPrefix(app.ApiVersion, "argoproj.io") && app.Kind == "Application" {
+				appCache[path] = app
+				if app.Spec.Source.Helm == nil {
+					resourceAppPathMap[app.Spec.Source.Path] = path
+				}
+			}
+		}
+		return nil
+	})
+
+	fmt.Println("Len appCache: ", len(appCache))
+	for k, v := range appCache {
+		fmt.Printf("%s: %s\n", k, v.Metadata.Name)
+	}
+	fmt.Println("Len resourceAppPathMap: ", len(resourceAppPathMap))
+	for k, v := range resourceAppPathMap {
+		fmt.Printf("%s: %s\n", k, v)
+	}
+
+	// Helm: Can only change Application, only has Application.yaml
+	// Kustomize:
+	// Plain yaml: Change can be:
+	//     1. Only in resources directory, need to find Application that specifies itself as path
+	//     2. Only in application yaml
+	//     3. Both resource and application yaml
+	//     All cases require locating the application yaml.
+
+	log.Println("--- Start mapping changes ---")
+	for _, p := range changedFiles {
+		// Find Application changes
+		if val, ok := appCache[p]; ok {
+			fmt.Println("Application changed: ", val.Metadata.Name)
+		} else {
+			// Find resource changes
+			dir, _ := path.Split(p)
+			dir = filepath.Clean(dir)
+			if val, ok := resourceAppPathMap[dir]; ok {
+				fmt.Println("Resource changed: ", appCache[val].Metadata.Name)
+			} else {
+				log.Println("!! ERROR: No application matched for path: ", dir)
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ikaruswill/argonaut
 
 go 1.16
 
-require github.com/go-git/go-git/v5 v5.4.2
+require (
+	github.com/go-git/go-git/v5 v5.4.2
+	gopkg.in/yaml.v2 v2.3.0
+)
